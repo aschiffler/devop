720f4a34597d658954e1711fdcada7ccada362d9
Andreas Schiffler <schiffler@roschi.eu>
2020-06-09 14:32:02 +0000

    updated for windows maschines

---
+++ postinstall.cmd
@@ -0,0 +1,5 @@
+(if not exist \node-red mkdir \node-red)
+(if not exist \node-red\uibuilder mkdir \node-red\uibuilder)
+(if not exist \node-red\uibuilder\ml5 mkdir \node-red\uibuilder\ml5)
+(if not exist \node-red\uibuilder\ml5\src mkdir \node-red\uibuilder\ml5\src)
+copy ml5\* \node-red\uibuilder\ml5\src\
--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,10 @@
 const {exec} = require('child_process');
 var command='sh postinstall.sh';
 
+if (process.platform == 'win32') {
+  command="postinstall.cmd";
+}
+
 const executedCommands = exec(command, (error, stdout) => {
     console.log(stdout);
     if (error) {
